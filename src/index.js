fe6df1b9c63aefa3dd506de28d6ce1c51a039079
Takuma Kakehi <tkakehi@takumas-mbp-2.home>
2019-06-16 16:12:33 -0400

    basic subscription resolver, and importing the resolver and PubSub library

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import Movie from './resolvers/Movie'
 import Actor from './resolvers/Actor'
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         Movie,
         Actor
     },
     context: {
-        db: db
+        db, 
+        pubsub
     }
 })
 
---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,9 @@
+const Subscription = {
+    movie: {
+        subscribe(parent, args, ctx, info){
+            
+        }
+    }
+}
+
+export { Subscription as default }
